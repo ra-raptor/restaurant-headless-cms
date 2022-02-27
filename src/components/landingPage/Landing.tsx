be32c8353e683c612c0e1ee21d163e42499b39ca
Vikas Kumar Saw <vikasvks0112@gmail.com>
2022-02-27 11:22:13 +0530

    landing page layout

---
+++ src/components/landingPage/Landing.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div>landing <img src="./../../../../assets/pattern.svg" alt="dd" /></div>
+    
+  )
+}
+
+export default Landing
