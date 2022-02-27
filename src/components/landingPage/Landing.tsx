4fe9e2aa06aa1f38eac2490cde376f8f042919db
Vikas Kumar Saw <vikasvks0112@gmail.com>
2022-02-27 18:51:05 +0530

    nav

--- src/components/landingPage/Landing.tsx
+++ src/components/landingPage/Landing.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import "./landing.scss"
 function Landing() {
   return (
-    <div>landing <img src="./../../../../assets/pattern.svg" alt="dd" /></div>
+    <div className='hero'>
+        <nav>
+            <p>Restaurant</p>
+            <ul>
+                <li className='active'>Home</li>
+                <li>Menu</li>
+                {/* <li><input type="text" /></li> */}
+                <li>Cart</li>
+            </ul>
+        </nav>
+        <main>
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, a?</h1>
+        </main>
+        
+    </div>
     
   )
 }
