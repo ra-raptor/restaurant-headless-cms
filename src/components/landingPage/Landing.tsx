80d26ae59b1a183614269ccac10be6c576d85d1f
Vikas Kumar Saw <vikasvks0112@gmail.com>
2022-02-27 19:20:14 +0530

    hero section

--- src/components/landingPage/Landing.tsx
+++ src/components/landingPage/Landing.tsx
@@ -13,7 +13,19 @@ function Landing() {
             </ul>
         </nav>
         <main>
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, a?</h1>
+            <article>
+                <p className='pre'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                {/* where taste meets tradiotion */}
+                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, a?</h1>
+                <p className='post'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus pariatur, officia ab sed doloremque non. Natus pariatur, officia ab sed doloremque non. Natus pariatur, officia ab sed doloremque non.</p>
+                <div className='cta'>
+                    <a href="#">ORDER NOW</a><a href='#'>VIEW MENU</a>
+                </div>
+
+                
+                </article>
+            <aside><h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, a?</h1></aside>
+            
         </main>
         
     </div>
