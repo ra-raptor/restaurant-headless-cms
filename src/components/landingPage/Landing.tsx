6ac77ac8c5a3fe7d5cdb974b19c7a24337b79d0e
Vikas Kumar Saw <vikasvks0112@gmail.com>
2022-03-12 12:43:00 +0530

    hero section completed

--- src/components/landingPage/Landing.tsx
+++ src/components/landingPage/Landing.tsx
@@ -1,46 +1,19 @@
 import React from 'react'
+import Hero from './hero'
+import {StaticImage} from 'gatsby-plugin-image'
 import "./landing.scss"
 function Landing() {
   return (
       <>
-    <div className='hero'>
-        <div className="wrapper-hero">
-            <nav>
-            <p>Chillax Canteen</p>
-            <ul>
-                <li className='active'>Home</li>
-                <li>Menu</li>
-                {/* <li><input type="text" /></li> */}
-                <li>Cart</li>
-            </ul>
-        </nav>
-        <main>
-            <article>
-                <p className='pre'>All You Need is Good Food and a Dream..</p>
-                {/* where taste meets tradiotion */}
-                <h1>A Day Without Cusine is Like a Day Without Sunshine.</h1>
-                <p className='post'>Chillax Canteen is the one stop shop for food, comfort and taste. It's a place where taste meets tradition.
-                Order, takeout or dine  the best Indian, Italian and French Cusine in town. 
-                 </p>
-                <div className='cta'>
-                    <a href="#"><div className='primary'>ORDER NOW</div></a><a href='#'><div className='secondary'>VIEW MENU</div></a>
-                </div>
-
-                
-                </article>
-            <aside>
-                {/* <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, a?</h1> */}
-            
-            <img width={300} src="" alt=""  />
-            </aside>
-            
-        </main>
-            </div>
-        
-        
-    </div>
+    <Hero />
+    
     {/* <div className="w1"></div> */}
-    <div className="filler"></div>
+    <div className="services">
+        <div className="sideimage">
+            <StaticImage  src="../../images/kiwi.png" alt="food" width={300} />
+        </div>
+        <article></article>
+    </div>
     <div className="two"></div>
     </>
   )
---
+++ src/components/landingPage/hero.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {StaticImage} from 'gatsby-plugin-image'
+function Hero() {
+  return (
+        <div className='hero'>
+        <div className="wrapper-hero">
+            <nav>
+            <p>Chillax Canteen</p>
+            <ul>
+                <li className='active'>Home</li>
+                <li>Menu</li>
+                {/* <li><input type="text" /></li> */}
+                <li>Cart</li>
+            </ul>
+        </nav>
+        <main>
+            <article>
+                <p className='pre'>All You Need is Good Food and a Dream..</p>
+                {/* where taste meets tradiotion */}
+                <h1>A Day Without Cusine is Like a Day Without Sunshine.</h1>
+                <p className='post'>Chillax Canteen is the one stop shop for food, comfort and taste. It's a place where taste meets tradition.
+                Order, takeout or dine  the best Indian, Italian and French Cusine in town. 
+                 </p>
+                <div className='cta'>
+                    <a href="#"><div className='primary'>ORDER NOW</div></a><a href='#'><div className='secondary'>VIEW MENU</div></a>
+                </div>
+
+                
+                </article>
+            <aside>
+                {/* <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, a?</h1> */}
+            <StaticImage src="../../images/ss2.png" alt="food" width={1200} />
+            {/* //<img width={300} src="" alt=""  /> */}
+            </aside>
+            
+        </main>
+        </div> 
+    </div>
+  )
+}
+
+export default Hero
