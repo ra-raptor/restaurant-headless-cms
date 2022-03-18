8bc35f4d5c76c1a0f097fe3732f4c2995601e6d3
Vikas Kumar Saw <vikasvks0112@gmail.com>
2022-03-18 23:36:38 +0530

    wave pattern

--- src/components/landingPage/Landing.tsx
+++ src/components/landingPage/Landing.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from './hero'
-
+import {StaticImage} from 'gatsby-plugin-image'
 import "./landing.scss"
 import Services from './services'
 
@@ -9,7 +9,9 @@ function Landing() {
     <>
         <Hero />
         <Services />
-    
+        <div className="wave3"> 
+        {/* <StaticImage alt="wave" src='../../images/wave3.svg' /> */}
+        </div>
         <div className="two"></div>
     </>
   )
