98eb65cbaf67c8c426b920c25080691b5682f4f0
ra-raptor <vikasvks0112@gmail.com>
2022-05-23 19:01:20 +0530

    search bar

--- src/components/menuPage/Hero.tsx
+++ src/components/menuPage/Hero.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import {Link} from 'gatsby'
 import './menu.scss'
 import { IoMdCart } from 'react-icons/io'
-import Map from '../landingPage/map'
+import { FaSearch } from 'react-icons/fa'
+
 
 function Hero() {
   return (
@@ -16,9 +17,22 @@ function Hero() {
                 <li><IoMdCart /></li>
             </ul>
         </nav>
-        <div className="banner">
-          {/* <Map /> */}
-        </div>
+        <section className='search'>
+          <div className="input-container">
+            <FaSearch />
+            <input type="text" placeholder='Search your favorite dish..'/>
+          </div>
+          <button>Search</button>
+        </section>
+        <main>
+          <div className="sidebar">
+            <div>Categories</div>
+            <div></div>
+            <div></div></div>
+          <section className='show'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem omnis nostrum dolorum molestiae consequuntur amet, minima quae incidunt perspiciatis voluptatibus rerum itaque nam perferendis. Corporis odit velit ipsum, maxime beatae eligendi ullam aliquid qui totam nesciunt doloribus error voluptatem vitae perferendis temporibus, sunt magnam, dolorum odio nam doloremque sit enim!
+          </section>
+        </main>
     </div>
   )
 }
