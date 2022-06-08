29852528ef6bb6ec32d30a4c0e1b547bcaa5d44e
ra-raptor <vikasvks0112@gmail.com>
2022-06-09 04:14:12 +0530

    cart functionality

--- src/components/cartPage/CartItem.tsx
+++ src/components/cartPage/CartItem.tsx
@@ -8,6 +8,8 @@ import { AiOutlinePlus,AiOutlineLine } from 'react-icons/ai'
 const CartItem = (data:CartDataInterface) => {
     const contextData:ContextInterface = useContext(GlobalContextData);
     const allFood = contextData.allFood;
+    const cartData = contextData.cartData;
+    const setcartData = contextData.setcartData;
 
     const currentFood = allFood.filter(food => food.id === data.id)[0]
 
@@ -16,6 +18,28 @@ const CartItem = (data:CartDataInterface) => {
     const vegnonveg = {
         color : currentFood.veg ? "green" : "red",
         border: currentFood.veg ? "1px solid green" : "1px solid red",
+    }
+
+    const addorsub = (action:string) => {
+        const food = currentFood
+        if(action==='ADD'){
+            const newPrice = cartData.filter(item => item.id===food.id)[0].totalPrice + food.price
+            setcartData(cartData.map(item => item.id===food.id ? {...item, count: data.count+1,totalPrice : newPrice } : item))
+        }
+        if(action==='SUB'){
+            
+            const newPrice = cartData.filter(item => item.id===food.id)[0].totalPrice - food.price
+            if(data.count===1){
+                setcartData(cartData.filter(item => item.id !== food.id))
+            }else{
+                setcartData(cartData.map(item => item.id===food.id ? {...item, count: data.count-1,totalPrice : newPrice } : item))
+            }
+            
+        }
+    }
+
+    const remove = () => {
+        setcartData(cartData.filter(item => item.id !== currentFood.id))
     }
 
   return (
@@ -31,14 +55,14 @@ const CartItem = (data:CartDataInterface) => {
         <div className="actions">
             <div className="quantity">
                 <div  className='cart-btns'>
-                    <div   className="del" ><AiOutlineLine /></div>
+                    <div   className="del"  onClick={() => addorsub('SUB')}><AiOutlineLine /></div>
                     <div className="count">{data.count}</div>
-                    <div  className="plus"><AiOutlinePlus /></div>
+                    <div  className="plus" onClick={() => addorsub('ADD')}><AiOutlinePlus /></div>
                 </div>
             </div>
             <div className="price">
                 <p className="totalPrice"> ₹ {data.totalPrice}</p>
-                <p className="remove">remove</p>
+                <p className="remove" onClick={remove}>remove</p>
             </div>
         </div>
     </div>
