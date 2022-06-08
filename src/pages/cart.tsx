419e8243699da97dc527b54803c974d1530a2403
ra-raptor <vikasvks0112@gmail.com>
2022-06-09 04:23:20 +0530

    ui

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,9 +1,10 @@
-import React,{useState,useContext, useEffect} from 'react'
+import React,{useContext} from 'react'
 import CartNav from '../components/cartPage/CartNav'
 import '../components/cartPage/cart.scss'
-import { ContextInterface, CartDataInterface, Food } from '../utils/Interface';
+import { ContextInterface} from '../utils/Interface';
 import { GlobalContextData } from '../context/GlobalContext';
 import CartItem from '../components/cartPage/CartItem';
+import { Link } from 'gatsby';
 const cart = () => {
     const contextData:ContextInterface = useContext(GlobalContextData);
     const cartData = contextData.cartData;
@@ -23,18 +24,26 @@ const cart = () => {
             <h2>Your Cart</h2>
             <p className='sub'>You have {cartData.length} items in your cart</p>
             {cartData.map((item) =>  <CartItem {...item}/>)}
-            <div className="checkout-wrapper">
-                <div className="checkout">
-                    <div className="price">
-                        <div className="left">
-                            <h4>Order Sub-Total</h4>
-                            <p>{cartData.length} items</p>
+            {cartData.length > 0 && (
+                <div className="checkout-wrapper">
+                    <div className="checkout">
+                        <div className="price">
+                            <div className="left">
+                                <h4>Order Sub-Total</h4>
+                                <p>{cartData.length} items</p>
+                            </div>
+                            <div className="right"> ₹ {calcTotal()}</div>
                         </div>
-                        <div className="right"> ₹ {calcTotal()}</div>
+                        <div className="btn" onClick={() => window.alert("thank you")} >Checkout</div>
                     </div>
-                    <div className="btn" onClick={() => window.alert("thank you")} >Checkout</div>
                 </div>
-            </div>
+            )}
+            {cartData.length === 0 && (
+                <Link to="/menu"><div className="to-menu">
+                MENU
+            </div></Link>
+            )}
+            
     </div>
         
     </>
